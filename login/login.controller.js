15ddfa8f261fbd806aaee1ba84127f16f5c99555
Tyler Moses <AF17783@LC02ND21LG3QN>
2017-03-28 15:47:45 -0400

    Added Node Server.

--- login/login.controller.js
+++ login/login.controller.js
@@ -1,19 +1,28 @@
 (function () {
     'use strict';
     angular
-    .module('app.common')
-    .controller('LoginController', 
-        [   
+        .module('app.common')
+        .controller('LoginController',
+        [
             '$location',
             'User',
-            function($location, User) {
+            '$firebaseAuth',
+            function ($location, User, $firebaseAuth) {
+                var auth = $firebaseAuth();
                 var vm = this;
                 vm.name = User.getName();
                 vm.submit = function () {
                     User.setName(vm.name);
                     return $location.url('hello');
+                    // Login with Facebook
+                    auth.$signInWithPopup("facebook").then(function (firebaseUser) {
+                        console.log("Signed in as:", firebaseUser.uid);
+                        console.log(firebaseUser);
+                    }).catch(function (error) {
+                        console.log("Authentication failed:", error);
+                    });
                 };
             }
         ]
-    );
+        );
 })();
